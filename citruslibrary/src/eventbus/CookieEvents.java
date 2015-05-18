cc086cf4f3ce54267e6d18c2120c1074ba336edf
Mangesh <mangesh.kadam@citruspay.com>
2015-05-18 18:25:16 +0530

    * event bus changes.

---
+++ citruslibrary/src/eventbus/CookieEvents.java
@@ -0,0 +1,40 @@
+/*
+ *
+ *    Copyright 2014 Citrus Payment Solutions Pvt. Ltd.
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ * /
+ */
+
+package eventbus;
+
+/**
+ * Created by MANGESH KADAM on 5/18/2015.
+ */
+public class CookieEvents {
+
+    private String cookie = null;
+
+    public CookieEvents(String cookie) {
+        this.cookie = cookie;
+    }
+
+    public void onCookieReceived(String cookie) {
+        this.cookie = cookie;
+    }
+
+    public void onCookieReceivedFailed() {
+        cookie = null;
+    }
+
+    public String getCookie() {
+        return cookie;
+    }
+}
