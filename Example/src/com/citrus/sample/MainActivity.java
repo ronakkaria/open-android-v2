0dee1ee7169328a63d3a8989c2665f1b0c229030
Salil Godbole <salilgodbole@gmail.com>
2015-06-06 17:40:09 +0530

    Added delete card functionality
    Conflicts:
    	Example/res/layout/activity_main.xml
    	Example/src/com/citrus/sample/MainActivity.java
    	citruslibrary/src/com/citrus/mobile/RESTclient.java

--- Example/src/com/citrus/sample/MainActivity.java
+++ Example/src/com/citrus/sample/MainActivity.java
@@ -21,6 +21,7 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import com.citrus.asynch.Binduser;
+import com.citrus.asynch.DeleteCard;
 import com.citrus.asynch.GetWallet;
 import com.citrus.asynch.Savecard;
 import com.citrus.card.Card;
@@ -29,16 +30,16 @@ import com.citrus.mobile.Config;
 
 public class MainActivity extends Activity {
 
-    Button bind, savecard, getWallet, paybutton, widgets;
+	Button bind, savecard, deletecard, getWallet, paybutton, logoutButton, widgetButton;
 
     Callback callback;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        init();
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		init();
 
         callback = new Callback() {
             @Override
@@ -51,52 +52,88 @@ public class MainActivity extends Activity {
 
         savecard = (Button) this.findViewById(R.id.savecard);
 
+		deletecard = (Button) this.findViewById(R.id.deletecard);
+
         getWallet = (Button) this.findViewById(R.id.getWallet);
 
-        paybutton = (Button) this.findViewById(R.id.paybutton);
-        
-        widgets = (Button) this.findViewById(R.id.widgets);
+		paybutton = (Button) this.findViewById(R.id.paybutton);
+		
+		logoutButton = (Button) this.findViewById(R.id.logoutbutton);
+		
+		widgetButton = (Button) this.findViewById(R.id.widgets);
+		
+		
 
-        bind.setOnClickListener(new View.OnClickListener() {
+		bind.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				new Binduser(MainActivity.this, callback).execute("tester46@gmail.com", "9020184710");
+			}
+		});
+
+		savecard.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Card card = new Card("4242424242424242", "12", "20", "123", "Bruce Wayne", "debit");
+				if(User.isUserLoggedIn(MainActivity.this))
+					new Savecard(MainActivity.this, callback).execute(card);
+				else
+					Toast.makeText(getApplicationContext(), "Bind the user before saving card details.", Toast.LENGTH_LONG).show();
+
+			}
+		});
+
+		deletecard.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				new DeleteCard(MainActivity.this, callback)
+						.execute(new String[]{"4242", "VISA"});
+			}
+		});
+
+		getWallet.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				if(User.isUserLoggedIn(MainActivity.this))
+					new GetWallet(MainActivity.this, callback).execute();
+				else
+					Toast.makeText(getApplicationContext(), "Bind the user to get wallet.", Toast.LENGTH_LONG).show();
+			}
+		});
+
+		paybutton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(MainActivity.this, PaymentPage.class);
+				startActivity(intent);
+			}
+		});
+		
+		logoutButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+			User.logoutUser(MainActivity.this);
+			if(User.logoutUser(MainActivity.this)) {
+				Toast.makeText(getApplicationContext(), "User Logged out successfully.", Toast.LENGTH_LONG).show();
+			}
+			else {
+				Toast.makeText(getApplicationContext(), "Failed to logout user.", Toast.LENGTH_LONG).show();
+			}
+			
+			}
+		});
+		
+		
+        widgetButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 new Binduser(MainActivity.this, callback).execute("shardullavekar123233443545@mailinator.com", "9873432991");
             }
         });
 
-        savecard.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Card card = new Card("4242424242424242", "12", "20", "123", "Bruce Wayne", "debit");
+	}
 
-                new Savecard(MainActivity.this, callback).execute(card);
-            }
-        });
-
-        getWallet.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                new GetWallet(MainActivity.this, callback).execute();
-            }
-        });
-
-        paybutton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, PaymentPage.class);
-                startActivity(intent);
-            }
-        });
-        
-        widgets.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, Widgets.class);
-                startActivity(intent);
-            }
-        });
-        
-    }
 
     private void init() {
         Config.setEnv("sandbox"); //replace it with "production" when you are ready
---
+++ citruslibrary/src/com/citrus/asynch/DeleteCard.java
@@ -0,0 +1,53 @@
+package com.citrus.asynch;
+
+import android.app.Activity;
+import android.os.AsyncTask;
+
+import org.json.JSONObject;
+
+import com.citrus.mobile.Callback;
+import com.citrus.mobile.Errorclass;
+import com.citrus.wallet.CardDelete;
+
+public class DeleteCard extends AsyncTask<String, Void, JSONObject>{
+	String cardlast4, scheme;
+	
+	Activity activity; Callback callback;
+	
+	public DeleteCard(Activity activity, Callback callback) {
+		this.activity = activity;
+		this.callback = callback;
+	}
+	
+	@Override
+	protected JSONObject doInBackground(String... params) {
+		
+		if (params.length < 2) {
+			return Errorclass.addErrorFlag("Please pass all the details", null);
+		}
+		
+		else {
+			this.cardlast4 = params[0];
+			this.scheme = params[1];
+			CardDelete cardDelete = new CardDelete(cardlast4, scheme);
+			
+			return cardDelete.deletecard(activity);
+		}
+		
+	}
+	
+	@Override
+	protected void onPostExecute(JSONObject result) {
+		super.onPostExecute(result);
+		
+		if (result.has("error")) {
+			callback.onTaskexecuted("", result.toString());
+		}
+		
+		else {
+			callback.onTaskexecuted(result.toString(), null);
+		}
+		
+	}
+	
+}
